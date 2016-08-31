33415a1d400a5bc4529b3d712c82478c3fb53b61
Dimitri <dimitri@pfaffenrodt.de>
2016-09-01 00:31:11 +0200

    cardboard impl extract renderer from MainActivity.

---
+++ cardboard/cardboard-impl/app/src/main/java/de/pfaffenrodt/cardboard/CardboardRenderer.java
@@ -0,0 +1,109 @@
+package de.pfaffenrodt.cardboard;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+import de.pfaffenrodt.opengl.common.Controller;
+import de.pfaffenrodt.opengl.common.MenuScene;
+
+/**
+ * Created by Dimitri on 01.09.16.
+ */
+public class CardboardRenderer implements GvrView.StereoRenderer {
+
+    public static final float CAMERA_Z = -.1f;
+
+    private Controller mController;
+    private Context mContext;
+
+    public CardboardRenderer(Context context) {
+        mContext = context;
+    }
+
+    public Controller getController() {
+        return mController;
+    }
+
+    public void setController(Controller controller) {
+        mController = controller;
+    }
+
+
+    private MenuScene mScene;
+
+
+    private final float[] mModelViewProjectionMatrix = new float[16];
+    private final float[] mHeadView = new float[16];
+    private final float[] mView = new float[16];
+    private final float[] mCamera = new float[16];
+    /**
+     * Prepares OpenGL ES before we draw a frame.
+     * @param headTransform The head transformation in the new frame.
+     */
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+        // Build the camera matrix and apply it to the ModelView.
+        Matrix.setLookAtM(
+                mCamera, 0,
+                0.0f, 0.0f, CAMERA_Z,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f);
+        headTransform.getHeadView(mHeadView, 0);
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+        // Draw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        // Apply the eye transformation to the camera.
+        Matrix.multiplyMM(mView, 0, eye.getEyeView(), 0, mCamera, 0);
+        float[] projectionMatrix = eye.getPerspective(.1f,10);
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mModelViewProjectionMatrix, 0, projectionMatrix, 0, mView, 0);
+        mController.draw(mModelViewProjectionMatrix);
+        if(mScene != null){
+            mScene.draw(mModelViewProjectionMatrix);
+        }
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    /**
+     * @param width New width of the surface for a single eye in pixels.
+     * @param height New height of the surface for a single eye in pixels.
+     */
+    @Override
+    public void onSurfaceChanged( int width, int height) {
+        /**
+         * update controller.
+         * controller will calculate center and use it to create an ray for picking
+         */
+        mController.setWidth(width);
+        mController.setHeight(height);
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        mScene = new MenuScene(mContext);
+        mController = new Controller(mScene);
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+}
--- cardboard/cardboard-impl/app/src/main/java/de/pfaffenrodt/cardboard/MainActivity.java
+++ cardboard/cardboard-impl/app/src/main/java/de/pfaffenrodt/cardboard/MainActivity.java
@@ -1,29 +1,16 @@
 package de.pfaffenrodt.cardboard;
 
 import android.content.res.Configuration;
-import android.opengl.GLES20;
-import android.opengl.Matrix;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
-import com.google.vr.sdk.base.Eye;
 import com.google.vr.sdk.base.GvrActivity;
 import com.google.vr.sdk.base.GvrView;
-import com.google.vr.sdk.base.HeadTransform;
-import com.google.vr.sdk.base.Viewport;
-
-import javax.microedition.khronos.egl.EGLConfig;
-
-import de.pfaffenrodt.opengl.common.Controller;
-import de.pfaffenrodt.opengl.common.MenuScene;
 
 public class MainActivity extends GvrActivity{
 
-    public static final float CAMERA_Z = -.1f;
-    public static final float ROOM_SCALE = 3f;
-
-    private Controller mController;
+    private CardboardRenderer mCardboardRenderer;
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -40,7 +27,8 @@ public class MainActivity extends GvrActivity{
     private void initializeGvrView() {
         GvrView gvrView = (GvrView) findViewById(R.id.gvr_view);
         // Associate a GvrView.StereoRenderer with gvrView.
-        gvrView.setRenderer(stereoRenderer);
+        mCardboardRenderer = new CardboardRenderer(this);
+        gvrView.setRenderer(mCardboardRenderer);
         gvrView.setTransitionViewEnabled(true);
         gvrView.setOnCardboardBackButtonListener(
                 new Runnable() {
@@ -52,79 +40,6 @@ public class MainActivity extends GvrActivity{
         // Associate the gvrView with this activity.
         setGvrView(gvrView);
     }
-
-    private final GvrView.StereoRenderer stereoRenderer = new GvrView.StereoRenderer() {
-
-        private MenuScene mScene;
-
-
-        private final float[] mModelViewProjectionMatrix = new float[16];
-        private final float[] mHeadView = new float[16];
-        private final float[] mView = new float[16];
-        private final float[] mCamera = new float[16];
-        /**
-         * Prepares OpenGL ES before we draw a frame.
-         * @param headTransform The head transformation in the new frame.
-         */
-        @Override
-        public void onNewFrame(HeadTransform headTransform) {
-
-            // Build the camera matrix and apply it to the ModelView.
-            Matrix.setLookAtM(
-                    mCamera, 0,
-                    0.0f, 0.0f, CAMERA_Z,
-                    0.0f, 0.0f, 0.0f,
-                    0.0f, 1.0f, 0.0f);
-            headTransform.getHeadView(mHeadView, 0);
-        }
-
-        @Override
-        public void onDrawEye(Eye eye) {
-            // Draw background color
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-            GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-            // Apply the eye transformation to the camera.
-            Matrix.multiplyMM(mView, 0, eye.getEyeView(), 0, mCamera, 0);
-            float[] projectionMatrix = eye.getPerspective(.1f,10);
-            // Calculate the projection and view transformation
-            Matrix.multiplyMM(mModelViewProjectionMatrix, 0, projectionMatrix, 0, mView, 0);
-            mController.draw(mModelViewProjectionMatrix);
-            if(mScene != null){
-                mScene.draw(mModelViewProjectionMatrix);
-            }
-        }
-
-        @Override
-        public void onFinishFrame(Viewport viewport) {
-
-        }
-
-        /**
-         * @param width New width of the surface for a single eye in pixels.
-         * @param height New height of the surface for a single eye in pixels.
-         */
-        @Override
-        public void onSurfaceChanged( int width, int height) {
-            /**
-             * update controller.
-             * controller will calculate center and use it to create an ray for picking
-             */
-            mController.setWidth(width);
-            mController.setHeight(height);
-        }
-
-        @Override
-        public void onSurfaceCreated(EGLConfig eglConfig) {
-            GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            mScene = new MenuScene(MainActivity.this);
-            mController = new Controller(mScene);
-        }
-
-        @Override
-        public void onRendererShutdown() {
-
-        }
-    };
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -145,8 +60,8 @@ public class MainActivity extends GvrActivity{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(mController != null){
-           return mController.onTouch(event);
+        if(mCardboardRenderer.getController() != null){
+           return mCardboardRenderer.getController().onTouch(event);
         }
         return false;
     }
