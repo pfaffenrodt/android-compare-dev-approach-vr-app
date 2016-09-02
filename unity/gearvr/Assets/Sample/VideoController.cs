e390e4477dd3f3128eaf621c86384ed00dea9e75
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2016-09-02 14:07:19 +0200

    update unity. disable MovieTexture on unity

--- unity/gearvr/Assets/Sample/VideoController.cs
+++ unity/gearvr/Assets/Sample/VideoController.cs
@@ -6,11 +6,15 @@ public class VideoController : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+#if !UNITY_ANDROID
+        // MovieTexture is not supported on mobile plattform.
+        // we have to create your own MovieTexture or buy one in assetstore
         ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+#endif
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
